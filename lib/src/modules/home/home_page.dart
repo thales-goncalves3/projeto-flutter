91cd3c1c8559a1bc82a6f31d48f92418c1aaf6bc
thales-goncalves3 <thalesgoncalves3@gmail.com>
2022-11-27 15:28:48 -0300

    Adicionando estilos a lista de usuários

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -25,19 +25,46 @@ class HomePage extends StatelessWidget {
       body: ListView.builder(
         itemCount: users.length,
         itemBuilder: (context, index) {
-          return Container(
-            decoration: const BoxDecoration(
-                border: Border(bottom: BorderSide(width: 1))),
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("ID: ${users[index].id}"),
-                  Text("Nome: ${users[index].username}"),
-                  Text("Email: ${users[index].email}"),
-                ],
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(5, 10, 5, 5),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.purple, width: 4),
+                borderRadius: BorderRadius.circular(3),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("ID: ${users[index].id}"),
+                          Text("Nome: ${users[index].username}"),
+                          Text("Email: ${users[index].email}"),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.update),
+                          color: Colors.purple,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.delete_outline),
+                          color: Colors.purple,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           );
